eeb7f1126b07fc1218f10cd3ccd2210bed8403f6
Julie Scherer <schererjulie96@gmail.com>
2023-04-30 18:27:20 -0500

    added backfill table to drop sql file

--- queries/0_drop.sql
+++ queries/0_drop.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS actors;
 DROP TYPE IF EXISTS films_array;
 DROP FUNCTION IF EXISTS get_quality;
 DROP TYPE IF EXISTS incremental_scd;
+DROP TABLE IF EXISTS actors_backfill_scd;
 DROP TABLE IF EXISTS actors_history_scd;
 DROP TYPE IF EXISTS qual_class;
